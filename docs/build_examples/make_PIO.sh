883ce7a057a9366f9df345f4715986204318155b
Colin James Grudzien <cgrudzien@ucsd.edu>
2024-08-29 10:12:17 -0700

    Cleaning artifacts, restructuring

--- docs/build_examples/make_PIO.sh
+++ docs/build_examples/make_PIO.sh
@@ -1,51 +1,62 @@
 #!/bin/bash
+#SBATCH --job-name=make_PIO
+#SBATCH --time=2:00:00
+#SBATCH --account=cwp168
 #SBATCH --partition=cw3e-shared
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=16
-#SBATCH -t 04:00:00
-#SBATCH --job-name="compile_pio"
-#SBATCH --output="compile_pio.%j.%N.out"
-#SBATCH --error="compile_pio.%j.%N.err"
-#SBATCH --account=cwp157
-#SBATCH --export=ALL
+#SBATCH --ntasks-per-node=8
+###############################################################################
+# module loads
+###############################################################################
+module purge
+module restore
+module load slurm
+module load cpu/0.17.3b
+module load intel/19.1.3.304/6pv46so
+module load intel-mkl/2020.4.304/vg6aq26
+module load intel-mpi/2019.10.317/ezrfjne
+module load cmake/3.21.4/bpzre3q
 
-# Original code by Caroline Papadopoulos 01/18/2024
-# Updated CJG 2024-05-02
-# compile script for compiling PIO2_5_8  
+###############################################################################
+# libaries are installed wth PREFIX as root and library name following
+###############################################################################
+export STACK="NETCDF_INTEL_INTELMPI"
+export PREFIX="/expanse/nfs/cw3e/cwp168/SOFT_ROOT/${STACK}"
+export HDF5="${PREFIX}/HDF5"
+export NETCDF="${PREFIX}/NETCDF"
+export PNETCDF="${PREFIX}/PNETCDF"
+export LD_LIBRARY_PATH="${PNETCDF}/lib:${NETCDF}/lib:${HDF5}/lib:${LD_LIBRARY_PATH}"
+export LD_RUN_PATH="${PNETCDF}/lib:${NETCDF}/lib:${HDF5_PATH}/lib:${LD_RUN_PATH}"
+export PATH="${NETCDF}/bin:${PATH}"
+export PIO="${PREFIX}/PIO"
 
-# Root directory for software compiles
-export SOFT_ROOT=/expanse/nfs/cw3e/cwp168/SOFT_ROOT
-export CC=mpiicc
-export FC=mpiifort
-export NETCDF=${SOFT_ROOT}/NETCDF
-export PNETCDF=${SOFT_ROOT}/NETCDF
-export PIO=${SOFT_ROOT}/ParallelIO-pio2_5_8
-
-printf "PIO is compiled at \n"
-printf "    ${PIO}\n"
+###############################################################################
+# Download PIO
+###############################################################################
+cd ${PREFIX}
 rm -rf ${PIO}
-
-# set up for environment for compile
-# netcdf and intel
-module load cpu/0.15.4
-module load intel/19.1.1.217
-module load intel-mpi/2019.8.254
-module load netcdf-c/4.7.4
-module load netcdf-fortran/4.5.3
-module load netcdf-cxx/4.2
-module load hdf5/1.10.6
-module load parallel-netcdf/1.12.1
-module load cmake/3.18.2
-
-module list
-
-cd ${SOFT_ROOT}
+rm -rf ParallelIO-pio2_5_8
+rm -f pio2_5_8.tar.gz
 wget https://github.com/NCAR/ParallelIO/archive/refs/tags/pio2_5_8.tar.gz
-tar -xf pio2_5_8.tar.gz
+tar -xvf pio2_5_8.tar.gz
 rm pio2_5_8.tar.gz
-cd ${PIO}
+
+###############################################################################
+# Compile / Test PIO
+###############################################################################
+cd ${PREFIX}/ParallelIO-pio2_5_8
 mkdir build; cd build
-cmake -DNetCDF_C_PATH=$NETCDF -DNetCDF_Fortran_PATH=$NETCDF -DPnetCDF_PATH=$PNETCDF -DCMAKE_INSTALL_PREFIX=${PIO} -DCMAKE_VERBOSE_MAKEFILE=1 -DPIO_ENABLE_TIMING=OFF ..
+
+# set compilers
+export CC=mpicc
+export FC=mpif90
+
+cmake -DNetCDF_C_PATH=$NETCDF -DNetCDF_Fortran_PATH=$NETCDF \
+  -DPnetCDF_PATH=$PNETCDF -DCMAKE_INSTALL_PREFIX=${PIO} \
+  -DCMAKE_VERBOSE_MAKEFILE=1 -DPIO_ENABLE_TIMING=OFF ..
+make -j 4
 make install
 make tests
+
+cd ${PREFIX}/ParallelIO-pio2_5_8/build
 ctest
